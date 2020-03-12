576eb88f37327eaa723507c97a1b64a92817f6d0
abs <abs@pineappledevelopers.com>
2020-03-12 02:12:55 +0100

    Design implementations and login design

---
+++ HouseOfCode.Web/scripts/Navbar.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 2) {
+            $('.navbar-custom').addClass('scrolled');
+        }
+        else {
+            $('.navbar-custom').removeClass('scrolled');
+        }
+    });
+});
+                        
